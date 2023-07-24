512900463b1ea93ab0e1b72cfe4c4056167dfc47
ToanNguyen2705 <toannguyen.27052001@gmail.com>
2023-07-24 17:50:58 +0700

    update

--- client/src/utils/request.jsx
+++ client/src/utils/request.jsx
@@ -1,17 +1,26 @@
 import { GRAPHQL_SERVER } from "./constants";
 
 export const graphQLRequest = async (payLoad, options = {}) => {
-  const res = await fetch(`${GRAPHQL_SERVER}/graphql`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      ...options,
-    },
-    body: JSON.stringify(payLoad),
-  });
+  if (localStorage.getItem("accessToken")) {
+    const res = await fetch(`${GRAPHQL_SERVER}/graphql`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        ...options,
+      },
+      body: JSON.stringify(payLoad),
+    });
 
-  const { data } = await res.json();
-  return data;
+    if(!res.ok){
+      if(res.stuts === 403){
+        return null;
+      }
+    }
+
+    const { data } = await res.json();
+    return data;
+  }
+  return null;
 };
